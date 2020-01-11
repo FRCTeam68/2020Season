e6d3e5caf0003fe9175a70901f1c7972ccc18a47
Ch1llze <connorjs100@gmail.com>
2020-01-10 20:18:27 -0500

    Fixed robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,7 +23,7 @@ import frc.robot.subsystems.*;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   
-  private RobotContainer m_robotContainer;
+  public static RobotContainer m_robotContainer;
 
   public static DriveTrain driveTrain;
   public static Vision vision;
